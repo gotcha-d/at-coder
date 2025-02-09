dd0fd1e7f5499171498b72afa4151a4bc12a97d4
gotcha-d <gotcha.privatemail@gmail.com>
2025-02-09 22:27:44 +0900

    Welcome to AtCoderts回答追加

--- PracticeA/main.ts
+++ PracticeA/main.ts
@@ -1,5 +1,11 @@
-const main = () => {
-  console.log("hello, world!")
+import * as fs from 'fs';
+
+const main = (input: string) => {
+
+  const lines: string[] = input.split(/\n/)
+  const sum = Number(lines[0]) + Number(lines[1].split(/\s/)[0]) + Number(lines[1].split(/\s/)[1])
+  console.log(sum, lines[2])
 }
 
-main()
+const input = fs.readFileSync("/dev/stdin", "utf8")
+main(input)
