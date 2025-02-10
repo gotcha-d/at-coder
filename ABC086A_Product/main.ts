0b95ce6ed5189106af484eb045c22f0c653100a0
gotcha-d <gotcha.privatemail@gmail.com>
2025-02-10 23:38:27 +0900

    NumberとparseInt()の違い検証

--- ABC086A_Product/main.ts
+++ ABC086A_Product/main.ts
@@ -9,5 +9,14 @@ function main(input: string): void {
   console.log(result)
 }
 
-const input = fs.readFileSync("/dev/stdin", "utf8")
-main(input)
+// const input = fs.readFileSync("/dev/stdin", "utf8")
+// main(input)
+
+// 数値のおまけ↓↓
+// parseIntは文字列のうち数値を判別する
+console.log(parseInt("1.1")) // 1
+console.log(parseInt("16px")) // 16
+
+// Numberは文字列全体を評価して数値に変換する。数値に変換できない場合はNaNを返す
+console.log(Number("1.1")) // 1.1
+console.log(Number("16px")) // NaN
