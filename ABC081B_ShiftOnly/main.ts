7755d6849c88875dc8f78d7d11ef20ebbd4888c5
gotcha-d <gotcha.privatemail@gmail.com>
2025-02-13 11:25:08 +0900

    ShiftOnly 回答誤りがあったので修正。1つでも偶数が含まれていれば操作継続してしまっていた

--- ABC081B_ShiftOnly/main.ts
+++ ABC081B_ShiftOnly/main.ts
@@ -1,18 +1,19 @@
 import * as fs from 'fs'
 
-function allEven(target: number[]): boolean {
+function allEven(target: number[], n: number): boolean {
   const evens = target.filter(value => {
     return value % 2 == 0
   })
-  return evens.length > 0
+  return evens.length == n
 }
 
 function main(inputs: string): void {
   const lines = inputs.split(/\n/)
+  const n = parseInt(lines[0])
   let candidates = lines[1].split(/\s/).map(element => parseInt(element))
 
   let count = 0
-  while (allEven(candidates)) {
+  while (allEven(candidates, n)) {
     count++
     const divided = candidates.map(item => item / 2 )
     candidates = divided
