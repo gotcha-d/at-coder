8876463c3e8b2a68101fbeb98b809f7b4cf14523
gotcha-d <gotcha.privatemail@gmail.com>
2025-02-13 11:34:39 +0900

    ShiftOnly Array.everyをつかってすっきりさせる

--- ABC081B_ShiftOnly/main.ts
+++ ABC081B_ShiftOnly/main.ts
@@ -1,19 +1,16 @@
 import * as fs from 'fs'
 
-function allEven(target: number[], n: number): boolean {
-  const evens = target.filter(value => {
-    return value % 2 == 0
-  })
-  return evens.length == n
+function allEven(target: number[]): boolean {
+  // every はコレクションの全ての要素が条件を満たしているか、真偽値を返す
+  return target.every((item) => item % 2 == 0 )
 }
 
 function main(inputs: string): void {
   const lines = inputs.split(/\n/)
-  const n = parseInt(lines[0])
   let candidates = lines[1].split(/\s/).map(element => parseInt(element))
 
   let count = 0
-  while (allEven(candidates, n)) {
+  while (allEven(candidates)) {
     count++
     const divided = candidates.map(item => item / 2 )
     candidates = divided
