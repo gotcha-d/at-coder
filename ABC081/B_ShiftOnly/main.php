19747236ed716fb09fe18a14cd994c9f09c273f8
gotcha-d <gotcha.privatemail@gmail.com>
2025-07-23 06:52:16 +0900

    ABC081 B問題回答コメント附記

--- ABC081/B_ShiftOnly/main.php
+++ ABC081/B_ShiftOnly/main.php
@@ -10,10 +10,12 @@ while ($line = trim(fgets(STDIN))) {
 
 $length = (int) $inputs[0][0];
 $numbers = array_map(fn($value) => (int) $value, $inputs[1]);
-$canContinue = true;
 $counter = 0;
 
+// 操作が行えなくなるまで操作を行う
 while (true) {
+    // 操作が行えるか判定を行う
+    $canContinue = true;
     for ($i = 0; $i < $length; $i++) {
         if (isEven($numbers[$i]) === false) {
             $canContinue = false;
@@ -21,10 +23,12 @@ while (true) {
         }
     }
 
+    // 操作が行えない場合反復を中断する
     if (!$canContinue) {
         break;
     }
 
+    // 操作が行える場合は操作する
     $numbers = array_map(fn($value) => $value / 2, $numbers);
     $counter++;
 }
