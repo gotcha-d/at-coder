3efd1e53fe40692a19251cd1bea6e0ab924f2b4f
gotcha-d <gotcha.privatemail@gmail.com>
2025-07-25 06:22:59 +0900

    ABC088 B問題 解答例をもとにアルゴリズム修正

--- ABC088/B_CardGameForTwo/main.php
+++ ABC088/B_CardGameForTwo/main.php
@@ -7,22 +7,20 @@ while ($line = trim(fgets(STDIN))) {
 
 $cardCount = $inputs[0][0];
 $cards = array_map(fn($val) => (int) $val, $inputs[1]);
-arsort($cards);
 
-$alice = 0;
-$bob = 0;
+// arsort, asortはキーと値の関係を維持するので不適。
+// キーと値の維持は不要なのでsortを使う
+rsort($cards);
 
-$turn = 1;
-while ($cardCount > 0) {
-    // 昇順にソート
-    if ($turn %  2 == 1) {
-        // array_shiftで配列の先頭を取得しつつ、配列から削除する
-        $alice += array_shift($cards);
-    } elseif ($turn % 2=== 0){
-        $bob += array_shift($cards);
+// Aliceのときは加算、Bobのときは減産すれば良い
+$result = 0;
+// 配列の0番目はAlice, 1番目はBob
+for ($i = 0; $i < $cardCount; $i++) {
+    if ($i % 2 == 0) {
+        $result += $cards[$i];
+    } else {
+        $result -= $cards[$i];
     }
-    $turn++;
-    $cardCount--;
 }
 
-print_r(($alice - $bob));
+print_r($result);
