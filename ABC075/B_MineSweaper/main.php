e067a91d287dc7298753f0a998659b91a1ac572b
gotcha-d <gotcha.privatemail@gmail.com>
2025-07-26 15:57:55 +0900

    ABC075 B問題 解答例をもとに修正

--- ABC075/B_MineSweaper/main.php
+++ ABC075/B_MineSweaper/main.php
@@ -1,15 +1,24 @@
 <?php
 
+// 下、右、上、左、右下、右上、左上、左下
+const DX = [0, 1, 0, -1, 1, 1, -1, -1];
+const DY = [1, 0, -1, 0, 1, -1, -1, 1];
+
+// 周囲8マスの'#'の個数を数える
 function check($y, $x, $maxHeight, $maxWidth, $strings) {
     $count = 0;
-    for ($i = -1; $i <= 1; $i++) {
-        for ($j = -1; $j <= 1; $j++) {
-            if (($y+$i < 0 || $y + $i > $maxHeight) || ($x+$j < 0 || $x+$j > $maxWidth)) {
-                continue;
-            }
-            if ($strings[$y+$i][$x+$j] == "#") {
-                $count++;
-            }
+    for ($d = 0; $d < 8; $d++) {
+        // マス(y, x)の周囲のマスを(ny, nx)とする
+        $nx = $x + DX[$d];
+        $ny = $y + DY[$d];
+
+        // マス(ny, nx)が盤面外の場合はスキップ
+        if ($nx < 0 || $nx >= $maxWidth || $ny < 0 || $ny >= $maxHeight) {
+            continue;
+        }
+
+        if ($strings[$ny][$nx] == '#') {
+            $count++;
         }
     }
     return $count;
@@ -28,7 +37,7 @@ for($i = 1; $i <= $height; $i++) {
 for ($i = 0; $i < $height; $i++) {
     for ($j = 0; $j < $width; $j++) {
         if ($strings[$i][$j] == '.') {
-            $count = check($i, $j, $height -1, $width - 1, $strings);
+            $count = check($i, $j, $height, $width, $strings);
             $strings[$i][$j] = $count;
         }
     }
