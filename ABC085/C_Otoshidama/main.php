e3f0bfc6b7ef250dddba47fb9eb338b9ffbfa7ac
gotcha-d <gotcha.privatemail@gmail.com>
2025-07-29 21:35:45 +0900

    ABC085 ヒントをもとに回答 AC

--- ABC085/C_Otoshidama/main.php
+++ ABC085/C_Otoshidama/main.php
@@ -4,19 +4,17 @@ const BILL_10000 = 10000;
 const BILL_5000 = 5000;
 const BILL_1000 = 1000;
 
-function sum($a, $b, $c) {
-    return $a + $b + $c;
-}
 fscanf(STDIN, "%s %s", $billCount, $totalAmount);
 
 for ($i = 0; $i <= $billCount; $i++) {
-    for($j = 0; $j <= $billCount; $j++) {
-        for ($k = 0; $k <= $billCount; $k++) {
-            $total = (BILL_10000 * $i) + (BILL_5000 * $j) + (BILL_1000 * $k);
-            if (($total == $totalAmount) && sum($i, $j, $k) == $billCount) {
-                printf("%d %d %d\n", $i, $j, $k);
-                return;
-            }
+    for($j = 0; $j <= $billCount - $i; $j++) {
+        // 10000円札と5000円札の枚数が分かれば1000円札の枚数も求まる
+        $k = $billCount - $i - $j;
+        $total = (BILL_10000 * $i) + (BILL_5000 * $j) + (BILL_1000 * $k);
+
+        if ($total == $totalAmount) {
+            printf("%d %d %d\n", $i, $j, $k);
+            return;
         }
     }
 }
