d03ce6fe4f964c3c91fe2c59704dd655a418efe9
gotcha-d <gotcha.privatemail@gmail.com>
2025-08-05 23:14:44 +0900

    第1回PAST F問題 模範回答をもとに回答AC

--- PAST1/F_DoubuleCamelCaseSort/main.php
+++ PAST1/F_DoubuleCamelCaseSort/main.php
@@ -6,16 +6,17 @@ $length = strlen($input);
 $chars = str_split($input);
 
 $words = [];
+// 文字列Sを単語ごとに分割する iは常に単語先頭の大文字を指す
 for ($i=0; $i < $length;){
-    $j = $i;
-    $wordElements[] = $chars[$j];
-    while ($j < $length && (count($wordElements) == 1 || !ctype_upper($chars[$j]))) {
+    // 初めてS[j]が大文字になるjを見つける
+    $j = $i + 1;
+    while ($j < $length && ctype_lower($chars[$j])) {
         $j++;
-        $wordElements[] = $chars[$j];
     }
+    // 単語を切り出す
+    $wordElements = array_slice($chars, $i, ($j - $i + 1));
     $words[] = implode("", $wordElements);
     $i = $j + 1;
-    $wordElements = [];
 }
 natcasesort($words);
 print_r(implode($words));
