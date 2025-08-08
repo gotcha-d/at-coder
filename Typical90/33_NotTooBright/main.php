4c8e9a52b8d52ad4153d69ce7cda0dad23540c17
gotcha-d <gotcha.privatemail@gmail.com>
2025-08-09 00:19:57 +0900

    典型90 33 解答例をもとに回答AC

--- Typical90/33_NotTooBright/main.php
+++ Typical90/33_NotTooBright/main.php
@@ -2,50 +2,11 @@
 
 [$height, $width] = array_map('intval', explode(" ", trim(fgets(STDIN))));
 
-function canPlace($board, $i, $j) {
-
-    $offsets = [
-        [0, 1],   // 右
-        [1, 1],   // 右下
-        [1, 0],   // 下
-        [1, -1],  // 左下
-        [0, -1],  // 左
-        [-1, -1], // 左上
-        [-1, 0],  // 上
-        [-1, 1]   // 右上
-    ];
-
-    foreach ($offsets as $position) {
-        $y = $position[0]; // y軸方向への補正
-        $x = $position[1]; // x軸方向への補正
-
-        if (isset($board[$i+$y][$j+$x]) && $board[$i+$y][$j+$x] == "#") {
-            return false;
-        }
-    }
-
-    return true;
+if ($height == 1 || $width == 1 ) {
+    // コーナーケース
+    $result = $height * $width;
+} else {
+    // i, j 両方奇数の位置に配置させるのが最適
+    $result = (intdiv($height +1, 2)) * (intdiv($width + 1, 2));
 }
-
-
-$board = [];
-
-// ボードの初期化
-for ($i = 0; $i < $height; $i++) {
-    $board[$i] = [];
-    for ($j = 0; $j < $width; $j++) {
-        $board[$i][$j] = ".";
-    }
-}
-
-$result = 0;
-for ($i = 0; $i < $height; $i++) {
-    for ($j = 0; $j < $width; $j++) {
-        if (canPlace($board, $i, $j)) {
-            $board[$i][$j] = "#";
-            $result++;
-        }
-    }
-}
-
-echo $result ."\n";
+echo $result . PHP_EOL;
