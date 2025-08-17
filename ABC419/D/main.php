16bfd498d1eddd93d474c731013303ab4dcf9ef2
gotcha-d <gotcha.privatemail@gmail.com>
2025-08-17 21:54:03 +0900

    ABC420 D問題の模範回答AC

--- ABC419/D/main.php
+++ ABC419/D/main.php
@@ -4,16 +4,37 @@
 $S_string = str_split(trim(fgets(STDIN)));
 $T_string= str_split(trim(fgets(STDIN)));
 
+// いもす法による回答
+// いもす法用の配列 最後の文字のフリップ判定のために文字列の長さは+1が必要
+$ims = array_fill(0, $n+1, 0);
+
+// 各クエリの範囲の開始と終了に細分を記録する
 for ($i = 1; $i <= $m; $i++) {
-    $from = 0;
-    $to = 0;
     [$from, $to] = explode(' ', trim(fgets(STDIN)));
 
-    $sSub = array_slice($S_string, $from -1, $to - $from + 1);
-    $tSub = array_slice($T_string, $from -1, $to - $from + 1);
+    // インデックスの位置を補正
+    $from--;
+    $to--;
 
-    array_splice($S_string, $from -1 , $to - $from +1, $tSub);
-    array_splice($T_string, $from -1 , $to - $from +1, $sSub);
+    $ims[$from] += 1;
+    $ims[$to+1] += -1;
 }
 
-echo implode('', $S_string) . PHP_EOL;
+// 累積和で実際の各位置での交換回数を計算
+for ($i = 1; $i < $n; $i++) { // 最初の1項は累積和を計算する必要がないので、開始位置は1
+    $ims[$i] += $ims[$i-1];
+}
+
+$ans = '';
+for ($i =0; $i < $n; $i++) {
+    if ($ims[$i] % 2 == 0) {
+        // 反転回数が偶数ならS
+        $ans .= $S_string[$i];
+    } else {
+        // 反転回数が奇数ならT
+        $ans .= $T_string[$i];
+    }
+}
+
+echo $ans;
+
---
+++ ABC419/D/main_example.php
@@ -0,0 +1,9 @@
+<?php
+
+[$n, $m] = explode(' ', trim(fgets(STDIN)));
+$S_string = str_split(trim(fgets(STDIN)));
+$T_string= str_split(trim(fgets(STDIN)));
+
+for ($i=1; $i <= $m; $i++) {
+    $for 
+}
