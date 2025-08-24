97ad61e00c600a0cff9414046653ada6e1ab16cb
gotcha-d <gotcha.privatemail@gmail.com>
2025-08-24 23:16:12 +0900

    ABC420 C問題 模範回答をもとに回答AC

--- ABC420/C.php
+++ ABC420/C.php
@@ -6,17 +6,24 @@
 $aString = array_map('intval', explode(' ', trim(fgets(STDIN))));
 $bString = array_map('intval', explode(' ', trim(fgets(STDIN))));
 
+$min = 0;
+for ($i = 0; $i < $N; $i++) {
+    $min += min($aString[$i], $bString[$i]);
+}
+
 for ($i = 1; $i <= $Q; $i++) {
     [$type, $targetIndexRaw, $new] = explode(' ', fgets(STDIN));
     $targetIndex = $targetIndexRaw -1;
+    $newVal = (int) $new;
+
+    // 入れ替える対象となる和を減算（あとで値を入れ替えて再計算する）
+    $min -= min($aString[$targetIndex], $bString[$targetIndex]);
     if ($type == 'A') {
         $aString[$targetIndex] = (int) $new;
     } elseif ($type == 'B') {
         $bString[$targetIndex] = (int) $new;
     }
-    $ans = 0;
-    for ($j = 0; $j < $N; $j++) {
-        $ans += min($aString[$j], $bString[$j]);
-    }
-    echo $ans .PHP_EOL;
+    // 和を再計算
+    $min += min($aString[$targetIndex], $bString[$targetIndex]);
+    echo $min . PHP_EOL;
 }
