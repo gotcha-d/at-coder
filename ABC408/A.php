03937a3419b2e07fa99d20467ca81ed664c32071
gotcha-d <gotcha.privatemail@gmail.com>
2025-09-21 22:24:23 +0900

    ABC408 A回答AC

---
+++ ABC408/A.php
@@ -0,0 +1,64 @@
+<?php
+
+// 上下左右への移動（下、右、上、左）を定義
+const DY = [1, 0, -1, 0];
+const DX = [0, 1, 0, -1];
+
+function printLn($arg): void
+{
+    echo $arg.PHP_EOL;
+}
+
+
+/**
+ * 文字列の入力を受け付ける
+ *
+ * @return string
+ */
+function inputString(): string
+{
+    return trim(fgets(STDIN));
+}
+
+/**
+ * スペース区切りで入力された文字列を、各要素配列にして返す
+ *
+ * @return array
+ */
+function inputStringsArray(): array
+{
+    return explode(" ", trim(fgets(STDIN)));
+}
+
+/**
+ * スペース区切りで入力された整数を、各要素配列にして返す
+ *
+ * @return array
+ */
+function inputIntegers(): array
+{
+    return array_map("intval", explode(" ", trim(fgets(STDIN))));
+}
+
+/**
+ * 入力された文字列を1文字ずつの配列に分解する
+ */
+function inputStringSplit(): array
+{
+    return str_split(trim(fgets(STDIN)));
+}
+
+[$n, $s] = inputIntegers();
+$t = inputIntegers();
+
+$old = 0;
+$limit = $s + 0.5;
+for ($i = 0; $i < $n; $i++) {
+    if ($t[$i] - $old > $limit) {
+        echo "No";
+        exit;
+    }
+    $old = $t[$i];
+}
+
+echo "Yes";
